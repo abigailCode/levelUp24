06751da7430dcc96db712ca07c76abf54f914613
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2024-07-02 19:36:17 +0100

    refactor: Control isActive in menu

--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -1,22 +1,47 @@
 using UnityEngine;
 
 public class MenuController : MonoBehaviour {
+    public MenuController menuController;
 
-    public void StartGame() => SCManager.instance.LoadScene("GameScene");
+    public void PerformAction(string action, string scene = "") {
+        if (!GameManager.instance.isActive)
+            return;
 
-    public void GoToSettings() => SCManager.instance.LoadScene("GeneralSettingsScene");
-
-    public void GoToRanking() => SCManager.instance.LoadScene("RankingScene");
-
-    public void GoToMenu() => SCManager.instance.LoadScene("MenuScene");
-
-    public void ReloadScene(string scene) => SCManager.instance.LoadScene(scene);
-
-    public void ExitGame() {
+        switch (action) {
+            case "StartGame":
+                SCManager.instance.LoadScene("GameScene");
+                break;
+            case "GoToSettings":
+                SCManager.instance.LoadScene("GeneralSettingsScene");
+                break;
+            case "GoToRanking":
+                SCManager.instance.LoadScene("RankingScene");
+                break;
+            case "GoToMenu":
+                SCManager.instance.LoadScene("MenuScene");
+                break;
+            case "LoadScene":
+                SCManager.instance.LoadScene(scene);
+                break;
+            case "ExitGame":
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
+                UnityEditor.EditorApplication.isPlaying = false;
 #elif UNITY_STANDALONE_WIN
-        Application.Quit();
+                Application.Quit();
 #endif
+                break;
+        }
     }
+
+    public void StartGame() => menuController.PerformAction("StartGame");
+
+    public void GoToSettings() => menuController.PerformAction("GoToSettings");
+
+    public void GoToRanking() => menuController.PerformAction("GoToRanking");
+
+    public void GoToMenu() => menuController.PerformAction("GoToMenu");
+
+    public void LoadScene(string scene) => menuController.PerformAction("LoadScene", scene);
+
+    public void ExitGame() => menuController.PerformAction("ExitGame");
 }
