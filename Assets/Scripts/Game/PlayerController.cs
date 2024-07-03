a401f05fb2d65e64f1cd5813bce9aa0b1223ee72
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2024-07-03 16:45:57 +0100

    feat: Add SyncController

--- Assets/Scripts/Game/PlayerController.cs
+++ Assets/Scripts/Game/PlayerController.cs
@@ -17,7 +17,7 @@ public class PlayerController : MonoBehaviour {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        _moveDirection = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
+        _rb.velocity = new Vector3(moveHorizontal * _speed, _rb.velocity.y, moveVertical * _speed);
 
         if (Input.GetButtonDown("Jump") && _isGrounded) {
             _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
@@ -25,14 +25,20 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
-    void FixedUpdate() {
-        if (!GameManager.instance.isActive) return;
-
-        Vector3 velocity = _moveDirection * _speed * Time.fixedDeltaTime;
-        _rb.MovePosition(_rb.position + velocity);
-    }
-
     void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.CompareTag("Ground")) _isGrounded = true;
+    }
+
+    public float GetSpeed() => _speed;
+
+    public void SetSpeed(float speed) => _speed = speed;
+
+    public float GetJumpForce() => _jumpForce;
+
+    public void SetJumpForce(float jumpForce) => _jumpForce = jumpForce;
+
+    public void SetAttributes(float speed, float jumpForce) {
+        _speed = speed;
+        _jumpForce = jumpForce;
     }
 }
--- Assets/Scripts/Game/SyncController.cs
+++ Assets/Scripts/Game/SyncController.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class SyncController : MonoBehaviour {
 
-    void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Player")) {
-            GetComponent<PlayerController>().enabled = true;
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            PlayerController mobController = GetComponent<PlayerController>();
+            PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+            mobController.enabled = true;
+            mobController.SetAttributes(playerController.GetSpeed(), playerController.GetJumpForce());
             GetComponent<Renderer>().material.color = Color.red;
+            Debug.Log("Contact!");
         }
     }
 }
