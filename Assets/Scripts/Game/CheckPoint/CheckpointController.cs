654fd10391574c8daf7dca51a5ce56e8842fb646
abigailojeda <abigail.arte@gmail.com>
2024-07-06 21:45:48 +0100

    checkpoints

---
+++ Assets/Scripts/Game/CheckPoint/CheckpointController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointController : MonoBehaviour
+{
+    [Header("|----------------Next Level Scene Name----------------|")]
+    public string nextLevel;
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collided with " + other.name);
+        if (other.CompareTag("Player"))
+        {
+            SCManager.Instance.LoadScene(nextLevel);
+        }
+    }
+}
